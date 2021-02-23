06a6603c2e3644fe2b5cbe806da0ffef161b6fbb
Zarwine <ange_550@hotmail.com>
2021-02-23 15:09:03 +0100

    Setting up Projet + Database ( Firebase )

---
+++ chatBox/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyA_rkIQ9dDGpmbZCx98XSHuhwksimPQTP0",
+    authDomain: "chatbox2-cf591.firebaseapp.com",
+    databaseURL: "https://chatbox2-cf591-default-rtdb.firebaseio.com/"
+});
+
+export default base;
---
+++ chatBox/src/components/Connexion.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {PropTypes} from 'prop-types';
+
+class Connexion extends React.Component {
+
+    goToChat = event => {
+        event.preventDefault();
+        const pseudo = this.pseudoInput.value;
+        this.props.history.push(`/pseudo/${pseudo}`);
+
+    };
+
+    render() {
+        return (
+            <div className="connexionBox" onSubmit={(e) => this.goToChat(e)}>
+                <form className="connexion">
+                    <input type="text" placeholder="Pseudo" required ref={input => {this.pseudoInput = input}}/>
+                    <button type="submit">Se connecter</button>
+                </form>
+            </div>
+        )
+    }
+
+    static contextTypes = {
+        router: PropTypes.object
+    }
+}
+
+export default Connexion;
