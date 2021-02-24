1c1df09b3789d7648b10031d5f1405dedb96e8af
Zarwine <ange_550@hotmail.com>
2021-02-24 10:38:00 +0100

    Final commit - Added PropTypes

--- chatBox/src/components/App.js
+++ chatBox/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Formulaire from './Formulaire';
 import Message from './Message';
 import base from '../base';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import '../animation.css';
 
 class App extends React.Component {
 
@@ -16,31 +18,52 @@ class App extends React.Component {
         });
     }
 
+    componentDidUpdate() {
+        this.messages.scrollTop = this.messages.scrollHeight;
+    }
+
     addMessage = message => {
         const messages = {...this.state.messages};
         const timestamp = Date.now();
         messages[`message-${timestamp}`]  = message;
+        Object.keys(messages).slice(0, -10).map(key => messages[key] = null);
         this.setState({ messages });
-    };
+    }
+
+    isUser = (pseudo) => {
+        return pseudo === this.props.match.params.pseudo;
+    }
 
     render() {
 
         const messages = Object
             .keys(this.state.messages)
-            .map(key => <Message key={key} details={this.state.messages[key]}/>)
+            .map(key => <Message key={key} details={this.state.messages[key]} isUser={this.isUser}/>)
         ;
 
         return (
             <div className="box">
                 <div>
-                    <div className="messages">
-                        {messages}
+                    <div className="messages" ref={input => this.messages = input}>
+                        <ReactCSSTransitionGroup
+                            component="div"
+                            className="message"
+                            transitionName="message"
+                            transitionEnterTimeout={200}
+                            transitionLeaveTimeout={200}
+                        >
+                            {messages}
+                        </ReactCSSTransitionGroup>
                     </div>
-                    <Formulaire addMessage={this.addMessage} pseudo={this.props.match.params.pseudo} length="140"/>
+                    <Formulaire addMessage={this.addMessage} pseudo={this.props.match.params.pseudo} length={140} />
                 </div>
             </div>
         )
     }
+
+    static propTypes = {
+        match: React.PropTypes.object.isRequired,
+    };
 }
 
 export default App;
